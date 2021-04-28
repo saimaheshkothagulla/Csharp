f5615715e553a70264c878a4f6fffd55e7c642b9
saimaheshkothagulla <62145782+saimaheshkothagulla@users.noreply.github.com>
2021-04-28 12:30:39 +0530

    Add files via upload

---
+++ ConstructorOverloading.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace CsharpIntermediateLevel
+{
+    class Student
+    {
+        public int Id;
+        public string Name;
+        public Student()
+        {
+
+        }
+        public Student(int id)
+        {
+            this.Id = id;
+
+        }
+        public Student(int val, string name)
+        {
+            this.Id = val;
+            this.Name = name;
+        }
+    }
+
+    class program
+    {
+        public static void Main(String[] args)
+        {
+            Student s1 = new Student();
+            s1.Id = 2;
+            s1.Name = "sai";
+            Student s2 = new Student(1, "mahesh");
+            Console.WriteLine(s2.Id);
+            Console.WriteLine(s2.Name);
+        }
+    }
+}
