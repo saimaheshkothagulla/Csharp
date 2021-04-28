b7d5baf2fffaf18f1565808b3b498ad3325cd937
saimaheshkothagulla <62145782+saimaheshkothagulla@users.noreply.github.com>
2021-04-28 11:12:25 +0530

    proporties in class(get and set)

---
+++ GetSetproperties.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace CsharpIntermediateLevel
+{
+    class Car
+    {
+        private string name = "bmw";//field
+        public string Name //class prpertiy
+        {
+            get{ return name; }
+            set{ name = value; }
+        }
+       
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Car c = new Car();
+            Console.WriteLine(c.Name);
+            c.Name = "royal";
+            Console.WriteLine(c.Name);
+
+        }
+    }
+}
