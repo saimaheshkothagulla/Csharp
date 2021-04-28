af8be7c28df17601399359e8b3ca320a26d76a02
saimaheshkothagulla <62145782+saimaheshkothagulla@users.noreply.github.com>
2021-04-28 17:41:10 +0530

    Add files via upload

---
+++ property (get,set)/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace CsharpIntermediateLevel
+{
+
+    class program
+    {
+        public static void Main(String[] args)
+        {
+            StudentAge s = new StudentAge();
+            s.BirthDay = new DateTime(1947, 6, 26);
+            Console.WriteLine(s.Age);
+
+   
+        }
+    }
+}
---
+++ property (get,set)/StudentAge.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace CsharpIntermediateLevel
+{
+    class StudentAge
+    {
+       public DateTime BirthDay { get; set; }
+       public int Age
+        {
+            get
+            {
+                var timeSpam = DateTime.Today - BirthDay;
+                var years = timeSpam.Days / 365;
+                return years;
+            }
+        }
+
+
+    }
+}
