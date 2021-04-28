82ec44b2337917cad106752f2a4b47858d6a2884
saimaheshkothagulla <62145782+saimaheshkothagulla@users.noreply.github.com>
2021-04-28 17:57:42 +0530

    Add files via upload

---
+++ without indexer/HttpCookie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+namespace WithOutIndexer
+{
+   public  class HttpCookie
+    {
+        private Dictionary<string, string> _dict=new Dictionary<string, string>();
+        public void SetItem(string key,string val)
+        {
+            _dict[key] = val;
+        }
+        public string GetItem(string key)
+        {
+            return _dict[key];
+        }
+
+
+
+    }
+}
---
+++ without indexer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace WithOutIndexer
+{
+
+    class program
+    {
+        public static void Main(String[] args)
+        {
+
+            var cookie = new HttpCookie();
+            cookie.SetItem("name", "saimahesh");
+            Console.WriteLine(cookie.GetItem("name"));
+   
+        }
+    }
+}
